13d02601f98862bfaa01f1f36e98c52c13bef580
Your Name <you@example.com>
2022-04-16 12:56:03 +0200

    Fix bug: unnecessary user creation when user_id is provided to factory

--- database/factories/NoteFactory.php
+++ database/factories/NoteFactory.php
@@ -18,7 +18,7 @@ class NoteFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => User::factory()->create(),
+            'user_id' => fn() => User::factory()->create(),  // fn() Trick to prevent NoteFactory to create a new user when user_id is provided.
             'body' => $this->faker->sentence(),
             'is_private' => false,
         ];
