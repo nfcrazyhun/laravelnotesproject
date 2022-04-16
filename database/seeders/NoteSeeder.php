54ff3a03c108cde082bf02a4830a51a72a83b361
Your Name <you@example.com>
2022-04-16 12:56:32 +0200

    Add NoteSeeder

---
+++ database/seeders/NoteSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+
+class NoteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * Seeding demo note data.
+     * @return void
+     */
+    public function run()
+    {
+
+
+        $admin = User::where(['username' => 'admin'])->first();
+
+
+        //Create a public note
+        Note::factory()->create(['user_id' => $admin]);
+
+
+        //Create a private note
+        Note::factory()->private()->create(['user_id' => $admin]);
+
+
+
+        //Create notes for users randomly
+        $users = User::all()->pluck('id')->toArray();
+
+        for ($i = 0; $i < 30; $i++) {
+            Note::factory()->create([
+                'user_id' => Arr::random($users),
+                'is_private' => rand(0,1),
+            ]);
+        }
+
+    }
+}
