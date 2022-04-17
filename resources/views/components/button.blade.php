de927bf6900c922a865607230293d322a5624f21
Your Name <you@example.com>
2022-04-17 11:16:33 +0200

    Update button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,4 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'px-4 py-2 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring']) }}>
+@props(['type' => 'submit'])
+<button {{ $attributes->merge(['type' => $type, 'class' => 'px-4 py-2 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring']) }}>
     {{ $slot }}
 </button>
