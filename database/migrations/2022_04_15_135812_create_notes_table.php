a50e77d913589505c6e7d839bc077f5dc7e86713
Your Name <you@example.com>
2022-04-17 14:06:37 +0200

    Add cascade delete

--- database/migrations/2022_04_15_135812_create_notes_table.php
+++ database/migrations/2022_04_15_135812_create_notes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(App\Models\User::class)->constrained();
+            $table->foreignIdFor(App\Models\User::class)->constrained()->cascadeOnDelete();
             $table->string('body')->nullable();
             $table->boolean('is_private')->default(false);
             $table->timestamps();
