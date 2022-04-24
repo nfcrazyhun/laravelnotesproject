16f7bf61f7b74c268855b365fc42a1c26017f48e
Your Name <you@example.com>
2022-04-24 18:36:41 +0200

    Update ApiController class

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,92 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ApiController extends Controller
 {
-    //
+    protected $statusCode = 200; // HTTP_OK, default
+
+    /**
+     * Gets the value of statusCode.
+     *
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * Sets the value of statusCode.
+     *
+     * @param mixed $statusCode the status code
+     *
+     * @return self
+     */
+    protected function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    public function responseNotFound($message = 'Not Found!') // 404
+    {
+        return $this->setStatusCode(Response::HTTP_NOT_FOUND)->respondWithError($message);
+    }
+
+    public function respondInvalidRequest($message = 'Invalid Request!') // 422
+    {
+        return $this->setStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY)->respondWithError($message);
+    }
+
+
+    public function respondInternalError($message = 'Internal Error') // 500
+    {
+        return $this->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR)->respondWithError($message);
+    }
+
+    public function respond($data, $headers = [])
+    {
+        return response()->json($data, $this->getStatusCode(), $headers);
+    }
+
+    protected function respondWithPagination(LengthAwarePaginator $lessons, array $data)
+    {
+
+        $data = array_merge($data, [
+            'paginator' => [
+                'total_count' => $lessons->total(),
+                'total_pages' => ceil($lessons->total() / $lessons->perPage()),
+                'current_page' => $lessons->currentPage(),
+                'limit' => $lessons->perPage()
+            ]
+        ]);
+
+        return $this->respond($data);
+    }
+
+    public function respondWithError($message)
+    {
+        return $this->respond([
+            'error' => [
+                'message' => $message,
+                'status_code' => $this->getStatusCode()
+            ]
+        ]);
+    }
+
+    public function respondWithMessage($message='All is well.')
+    {
+        return $this->respond([
+            'response' => [
+                'message' => $message,
+                'status_code' => $this->getStatusCode()
+            ]
+        ]);
+    }
+
+
 }
