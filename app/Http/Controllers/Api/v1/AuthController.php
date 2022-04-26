7530abf93acbc7acdf0d974a288216cb97c4a585
Your Name <you@example.com>
2022-04-26 21:12:50 +0200

    Add api v1 AuthController

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Validator;
+
+
+class AuthController extends ApiController
+{
+
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['username' => $request->username, 'password' => $request->password])){
+            $auth = Auth::user();
+            $success['token'] =  $auth->createToken('LaravelSanctumAuth')->plainTextToken;
+            $success['name'] =  $auth->name;
+
+            return $this->respond($success);
+        }
+        else{
+            return $this->responseNotFound('Authentication failed.');
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()){
+            return $this->handleError($validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('LaravelSanctumAuth')->plainTextToken;
+        $success['name'] =  $user->name;
+
+        return $this->handleResponse($success, 'User successfully registered!');
+    }
+
+}
