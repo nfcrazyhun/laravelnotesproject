e985537391cce6f7b591b99a38630a4e756ac0c1
Your Name <you@example.com>
2022-04-27 19:27:27 +0200

    Add api v1 logout route

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 // Api login and register
 Route::name('api.v1.')->group(function () {
     Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('register', [AuthController::class, 'register'])->name('register');
 });
 
