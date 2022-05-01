c2e0586bdcbfe09d4d5b6ef0630e445c11c1dd63
Your Name <you@example.com>
2022-05-01 11:18:52 +0200

    Update RegistrationTest

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -3,12 +3,23 @@
 namespace Tests\Feature\Auth;
 
 use App\Providers\RouteServiceProvider;
+use App\Services\UserService;
+use http\Client\Curl\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected UserService $userService;
+
+    public function setUp(): void
+    {
+        $this->userService = new UserService;
+
+        parent::setUp();
+    }
 
     public function test_registration_screen_can_be_rendered()
     {
@@ -17,13 +28,31 @@ class RegistrationTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_new_users_can_register()
+    public function test_new_users_cannot_register_without_invitation_code()
     {
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+        ]);
+
+        $this->assertGuest();
+    }
+
+    public function test_new_users_can_register_with_invitation_code()
+    {
+        $user = \App\Models\User::factory()->create();
+
+        $pendingUser = $this->userService->createPendingUser($user->id);
+
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'username' => 'tesuserke',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'invitation_code' => $pendingUser->invitation_code,
         ]);
 
         $this->assertAuthenticated();
