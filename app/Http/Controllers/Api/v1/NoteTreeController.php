62a2f8af947194e20d7ff2f2257f557acf5efc25
Your Name <you@example.com>
2022-05-01 18:42:46 +0200

    Add api docs for NoteTreeController

--- app/Http/Controllers/Api/v1/NoteTreeController.php
+++ app/Http/Controllers/Api/v1/NoteTreeController.php
@@ -7,6 +7,13 @@ use App\Http\Transformers\NoteTransformer;
 use App\Services\NoteTreeService;
 use Illuminate\Http\Request;
 
+/**
+ * @group NotesTree
+ *
+ * APIs to lists Notes Tree
+ *
+ * @authenticated
+ */
 class NoteTreeController extends ApiController
 {
     protected NoteTransformer $noteTransformer;
@@ -19,9 +26,39 @@ class NoteTreeController extends ApiController
     /**
      * Display a listing of the resource.
      *
-     * Descrription1
+     * List other users public (and your other) notes under your hierarchy
      *
      * @queryParam username string
+     *
+     * @response 200 {
+        "0": [
+            {
+                "id": 1,
+                "user_id": 1,
+                "body": "Consequatur dignissimos itaque nostrum explicabo illo.",
+                "status": 2
+            },
+            {
+                "id": 2,
+                "user_id": 1,
+                "body": "post form api updated",
+                "status": 1
+            }
+        ],
+        "paginator": {
+            "total_count": 2,
+            "total_pages": 1,
+            "current_page": 1,
+            "limit": 15
+        }
+    }
+     *
+     * @response 404 {
+        "error": {
+            "message": "Not Found!",
+            "status_code": 404
+        }
+    }
      *
      * @return \Illuminate\Http\JsonResponse
      *
