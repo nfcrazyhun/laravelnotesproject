fce3384f24dbfee10251f819dd25ee18587e5bd1
Your Name <you@example.com>
2022-05-01 18:59:39 +0200

    Add api docs for UserController

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\ApiController;
 use App\Http\Transformers\UserTransformer;
-use App\Models\User;
-use Illuminate\Http\Request;
 
+/**
+ * @group Users
+ *
+ * APIs to list users under your hierarchy
+ *
+ * @authenticated
+ */
 class UserController extends ApiController
 {
     protected $userTransformer;
@@ -18,6 +23,31 @@ class UserController extends ApiController
 
     /**
      * Display a listing of the resource.
+     *
+     * @response 200 {
+    "0": [
+            {
+                "id": 1,
+                "name": "Ms. Anderson",
+                "username": "admin",
+                "email": "admin@admin.com",
+                "parent_id": null
+            },
+            {
+                "id": 5,
+                "name": "Chester Schaefer",
+                "username": "beatrice.bahringer",
+                "email": "lavada53@example.net",
+                "parent_id": 1
+            }
+        ],
+        "paginator": {
+            "total_count": 2,
+            "total_pages": 1,
+            "current_page": 1,
+            "limit": 15
+        }
+    }
      *
      * @return \Illuminate\Http\Response
      */
