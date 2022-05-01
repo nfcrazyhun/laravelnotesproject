3d517405ab80882612d2e8b7f44eba67f02c536c
Your Name <you@example.com>
2022-05-01 21:52:17 +0200

    Add api AuthTest

---
+++ tests/Feature/api/AuthTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\api;
+
+use App\Models\User;
+use Tests\ApiTester;
+
+class AuthTest extends ApiTester
+{
+    public function test_registration()
+    {
+        //$this->actingAs($user, 'api')->...
+
+        //todo api tests
+    }
+}
